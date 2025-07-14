5ebc89853ffb7da95fc742ca5c14c02625a7e23a
Anirudh Singh Air <83878255+bodsvei@users.noreply.github.com>
2025-07-14 12:22:37 +0530

    opencv setup

---
+++ build/CMakeFiles/Vulcan.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Vulcan.dir/main.cpp.o"
+  "CMakeFiles/Vulcan.dir/main.cpp.o.d"
+  "Vulcan"
+  "Vulcan.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Vulcan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
 
-
-using namespace as std;
+using namespace std;
+using namespace cv;
 
 int main(){
-
+    VideoCapture cap(0);
+    while (true){
+        Mat frame;
+        cap.read(frame);
+        imshow("Camera", frame);
+        if(waitKey(30)==27){
+            return 0;
+        }
+    }
 }
